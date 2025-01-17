012ec6ccdcd3306293bf2ff9846d521e3f034a78
DavidSpartan95 <davidulwhan@hotmail.com>
2025-01-17 11:27:19 +0100

    Prevents user from using slideshow without any images

--- app/src/main/java/com/davidspartan/pictureviewer/view/screenA/ScreenA.kt
+++ app/src/main/java/com/davidspartan/pictureviewer/view/screenA/ScreenA.kt
@@ -1,11 +1,14 @@
 package com.davidspartan.pictureviewer.view.screenA
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,11 +18,15 @@ import com.davidspartan.pictureviewer.view.ScreenB
 import com.davidspartan.pictureviewer.view.SlideShowScreen
 import com.davidspartan.pictureviewer.viewmodel.ImageViewModel
 
+
 @Composable
 fun ScreenA(
     navController: NavHostController,
     viewModel: ImageViewModel
 ) {
+
+    val images by viewModel.images.collectAsState()
+    val context = androidx.compose.ui.platform.LocalContext.current
 
     Column(
         modifier = Modifier.fillMaxSize().padding(5.dp),
@@ -29,7 +36,12 @@ fun ScreenA(
         OptionButton(
             text = "Slideshow",
             onClick = {
-                navController.navigate(SlideShowScreen)
+                if (images.isNotEmpty()){
+                    navController.navigate(SlideShowScreen)
+                }else{
+                    Toast.makeText(context, "No Pictures stored", Toast.LENGTH_SHORT).show()
+                }
+
             }
         )
         Spacer(modifier = Modifier.padding(5.dp))
